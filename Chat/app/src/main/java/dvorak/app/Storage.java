4ba6bc068a6bbcfab9702a35d4388ca7e6bdf70f
Bill Ni <bni@stuy.edu>
2018-11-06 11:49:02 -0500

    finished storage api

--- Chat/app/src/main/java/dvorak/app/Storage.java
+++ Chat/app/src/main/java/dvorak/app/Storage.java
@@ -1,14 +1,92 @@
 package dvorak.app;
 
-import android.content.Context;
+import android.content.ContentValues;
+import java.util.ArrayList;
 import android.database.Cursor;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class Storage {
 
+    public void write(String usr, String msg){
+
+        SQLiteOpenHelper helper = open();
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("sender", usr);
+        cv.put("content", msg);
+
+        db.insert("msgs", null, cv);
+
+    }
+
     public void write(String msg){
+
+        SQLiteOpenHelper helper = open();
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("sender", "public");
+        cv.put("content", msg);
+
+        db.insert("msgs", null, cv);
+
+    }
+
+    public ArrayList<String> read(String usr){
+
+        SQLiteOpenHelper helper = open();
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT content FROM msgs WHERE sender = '" + usr + "'", null);
+
+        ArrayList<String> rtrnArr = new ArrayList<String>();
+        while(c.moveToNext()){
+            rtrnArr.add(c.getString(0));
+        }
+
+        return rtrnArr;
+
+    }
+
+    public void erase(String usr, String msg){
+
+        SQLiteOpenHelper helper = open();
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        db.execSQL("DELETE FROM msgs WHERE sender = '" + usr + "' AND content = '" + msg + "'");
+
+    }
+
+    public void purge(){
+
+        SQLiteOpenHelper helper = open();
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        db.execSQL("DELETE FROM msgs");
+
+    }
+
+    public ArrayList<String> usrs(){
+
+        SQLiteOpenHelper helper = open();
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT sender FROM msgs", null);
+
+        ArrayList<String> rtrnArr = new ArrayList<String>();
+        while(c.moveToNext()){
+            rtrnArr.add(c.getString(0));
+        }
+
+        return rtrnArr;
+
+    }
+
+    public SQLiteOpenHelper open(){
 
         SQLiteOpenHelper helper = new SQLiteOpenHelper(null, "msgs", null, 1, null) {
             @Override
@@ -23,8 +101,7 @@ public class Storage {
             }
         };
 
-        SQLiteDatabase db = helper.getWritableDatabase();
+        return helper;
 
     }
-
 }
