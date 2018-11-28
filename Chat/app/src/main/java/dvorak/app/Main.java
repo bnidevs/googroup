a21101718a0c13268a4049fb2a01985ad1f93d79
Josh Weiner <joshuaweinernyc@gmail.com>
2018-11-28 16:42:58 -0500

    fixing dependency issues'

--- Chat/app/src/main/java/dvorak/app/Main.java
+++ Chat/app/src/main/java/dvorak/app/Main.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.EditText;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.*;
+import android.content.BroadcastReceiver;
 
 public class Main extends AppCompatActivity {
 
@@ -122,6 +127,8 @@ public class Main extends AppCompatActivity {
         super.onResume();
         registerReceiver(mReceiver, mIntentFilter);
     }
+
+
 
     /* 003 */
     protected void onPause() {
